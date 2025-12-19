ca1e5e3adb1fa5679d9e26a9bf3cd1a443aa90f9
kemal <kemalyen@gmail.com>
2025-12-19 17:11:58 +0000

    default appname is updated

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('Corvus App')
+            ->brandName(config('app.name'))
             ->colors([
                 'primary' => Color::Neutral,
                 'tertiary' => Color::Gray,
